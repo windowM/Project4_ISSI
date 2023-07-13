fb704f6daf191ff452227cccebba545d165ff06c
MoonChangHo <tony9911@naver.com>
2023-07-14 02:25:21 +0900

    create Member.class
    멤버에대한 정보를 담는 자바 파일

--- src/PRTest.java
+++ src/PRTest.java
@@ -1,5 +1,11 @@
 public class PRTest {
     public static void main(String[] args) {
+
+        Member Lee = new Member();
+        Lee.setAge(18);
+        Lee.setId("aaaa@naver.com");
+        Lee.setName("aaaa");
+
         System.out.println("Hello PR");
 	    System.out.println("haha");
         System.out.println("abc");
