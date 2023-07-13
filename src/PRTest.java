391e23b934b259fe2fdc5b0743e0b7cc6928ed7a
서재용 <wodyd3215@naver.com>
2023-07-14 02:35:45 +0900

    car information
    자동차에 대한 정보

--- src/PRTest.java
+++ src/PRTest.java
@@ -1,6 +1,10 @@
 public class PRTest {
     public static void main(String[] args) {
-        System.out.println("Hello PR");
+    	Car car = new Car();
+    	car.color = "red";
+    	car.type = "truck";
+    	
+    	System.out.println("Hello PR");
 	    System.out.println("haha");
         System.out.println("abc");
         System.out.println("cjekcs");
