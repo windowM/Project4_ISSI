2be073fcb8ad06ed93c0d1414479e172d484eecf
이다현 <eda0227@naver.com>
2023-07-14 02:46:11 +0900

    animal

--- src/PRTest.java
+++ src/PRTest.java
@@ -1,5 +1,11 @@
+import animal.Animal;
+
 public class PRTest {
     public static void main(String[] args) {
+        Animal animal = new Amimal();
+        Animal.name = "cong";
+        Animal.color = "blue";
+
         System.out.println("Hello PR");
 	    System.out.println("haha");
         System.out.println("abc");
